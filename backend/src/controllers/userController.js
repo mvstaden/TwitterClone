22be73153e23c75fcb38f1a63c64cc34c4336b50
Marius Van Staden <mvstaden91@gmail.com>
2025-03-01 13:24:22 +0200

    profileimg and coverimg uplaod to cloudinary

--- backend/src/controllers/userController.js
+++ backend/src/controllers/userController.js
@@ -1,6 +1,7 @@
 import Notification from "../models/notificationModel.js";
 import User from "../models/userModel.js";
 import bcrypt from "bcryptjs";
+import { v2 as cloudinary } from "cloudinary";
 
 export const getUserProfile = async (req, res) => {
   const { username } = req.params;
@@ -100,6 +101,7 @@ export const getSuggestedUsers = async (req, res) => {
 };
 export const updateUser = async (req, res) => {
   //Get values we want to update
+
   const { fullName, email, username, currentPassword, newPassword, bio, link } =
     req.body;
 
@@ -109,11 +111,11 @@ export const updateUser = async (req, res) => {
   const userId = req.user._id;
 
   try {
-    const user = await User.findById(userId);
+    let user = await User.findById(userId);
     if (!user) return res.status(404).json({ message: "User not found" });
     if (
-      (!newPassword && !currentPassword) ||
-      (!currentPassword && !newPassword)
+      (!newPassword && currentPassword) ||
+      (!currentPassword && newPassword)
     ) {
       return res.status(400).json({
         message: "Please provide both current password and new password",
@@ -121,18 +123,60 @@ export const updateUser = async (req, res) => {
     }
     if (currentPassword && newPassword) {
       const isMatch = await bcrypt.compare(currentPassword, user.password);
-      if (!isMatch)
+      if (!isMatch) {
         return res
           .status(400)
           .json({ message: "Current password is incorrect" });
-    }
-    if (newPassword.length < 6) {
-      return res
-        .status(400)
-        .json({ message: "Password must be at least 6 characters long" });
+      }
+      if (newPassword.length < 6) {
+        return res
+          .status(400)
+          .json({ message: "Password must be at least 6 characters long" });
+      }
+      const salt = await bcrypt.genSalt(10);
+      user.password = await bcrypt.hash(newPassword, salt);
     }
 
-    const salt = await bcrypt.genSalt(10);
-    user.password = await bcrypt.hash(newPassword, salt);
-  } catch (error) {}
+    //save profile & cover image to cloudinary
+    if (profileImg) {
+      //Removes profile image from cloudinary
+      if (user.profileImg) {
+        await cloudinary.uploader.destroy(
+          user.profileImg.split("/").pop().split(".")[0]
+        );
+      }
+      //Add new profile image to cloudinary
+      const uploadedProfileImg = await cloudinary.uploader.upload(profileImg);
+      profileImg = uploadedProfileImg.secure_url;
+    }
+    if (coverImg) {
+      //Removes current coverImg from cloudinary
+      if (user.coverImg) {
+        await cloudinary.uploader.destroy(
+          user.coverImg.split("/").pop().split(".")[0]
+        );
+      }
+      //Adds new coverImg to cloudinary
+      const uploadedCoverImg = await cloudinary.uploader.upload(coverImg);
+      profileImg = uploadedCoverImg.secure_url;
+    }
+
+    //update user
+    user.fullName = fullName || user.fullName;
+    user.email = email || user.email;
+    user.username = username || user.username;
+    user.bio = bio || user.bio;
+    user.link = link || user.link;
+    user.profileImg = profileImg || user.profileImg;
+    user.coverImg = coverImg || user.coverImg;
+
+    user = await user.save();
+    //This just sends password as null to response
+    user.password = null;
+
+    return res.status(200).json(user);
+  } catch (error) {
+    console.log(`Error updating user: ${error.message}`);
+    res.status(500).json({ message: `Server error,${error.message}` });
+  }
 };
--- backend/src/server.js
+++ backend/src/server.js
@@ -3,15 +3,22 @@ import express from "express";
 import "dotenv/config";
 import cors from "cors";
 import cookieParser from "cookie-parser";
-
+import { v2 as cloudinary } from "cloudinary";
 //API imports
-
-//DB import
-import connectMongoDB from "./db/mongoDB.js";
 import authRoutes from "./routes/authRoutes.js";
 import userRoutes from "./routes/userRoutes.js";
 
+//DB import
+import connectMongoDB from "./db/mongoDB.js";
+
 const app = express();
+
+//CloudinaryConfig
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
 
 app.use(cors());
 app.use(express.json());
